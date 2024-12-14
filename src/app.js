07ce59b5d67b1f7eb5fb10a6aeb8af48735f074b
MenuchiFeldman <m0548488576@gmail.com>
2024-12-14 19:23:47 +0200

    change

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ function App() {
   const [message, setMessage] = useState('');
 
   const fetchData = () => {
-    fetch('http://34.8.85.115:80:5000/api')
+    fetch('http://34.8.85.115:5000/api')
       .then(response => response.json())
       .then(data => setMessage(data.message))
       .catch(error => console.error('Error fetching data:', error));
